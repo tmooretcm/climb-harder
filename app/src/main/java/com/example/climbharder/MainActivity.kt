fd082df8f74e0d64a2f04cfd2bfb43f35f781443
Aryan Parikh <aryanparikh@Aryans-MacBook-Air.local>
2022-10-27 01:23:15 -0500

    prototype

---
+++ app/src/main/java/com/example/climbharder/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.climbharder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.climbharder.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setCurrentFragment(fragmentAddWorkout())
+
+        binding.navView.setOnItemSelectedListener { item ->
+            when (item.itemId){
+                R.id.icon_log -> {
+                    setCurrentFragment(fragmentLog())
+                    true
+                }
+                R.id.icon_new -> {
+                    setCurrentFragment(fragmentAddWorkout())
+                    true
+                }
+                else -> {
+                    setCurrentFragment(fragmentSchedule())
+                    true
+                }
+            }
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frameLayout, fragment)
+            commit()
+        }
+
+}
