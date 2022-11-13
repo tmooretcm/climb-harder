3e2adcc8232344266f03007fba4d0617a6ecddd5
Thomas Moore <tmooretcm@utexas.edu>
2022-11-13 15:06:42 -0600

    clean up repo

--- app/build/generated/data_binding_trigger/debug/amc/g11/climbharder/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package amc.g11.climbharder;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
--- app/build/generated/source/kapt/debug/amc/g11/climbharder/BR.java
+++
@@ -1,5 +0,0 @@
-package amc.g11.climbharder;
-
-public class BR {
-  public static final int _all = 0;
-}
